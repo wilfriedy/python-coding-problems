91e01e5f212f752e64646193cf667e95656aba47
boydeveloper <wilfriedakpe3@gmail.com>
2022-02-23 15:44:52 +0100

    conditional expression modifications

--- averagePairs.py
+++ averagePairs.py
@@ -23,7 +23,7 @@ def averagePair(arr , targ) :
     start = 0
     def average(val1 , val2):
        return (val1 + val2) / 2
-    if (len(arr) <= 0 ) : 
+    if (len(arr) < 1 ) : 
         return False
     for i in range(1, arrLength):
         
@@ -37,6 +37,6 @@ def averagePair(arr , targ) :
 # print(averagePair([12,4,5,6], 4.5))
 
 # print(averagePair([1,2,3],2.5) ) #true
-print(averagePair([1,3,3,5,6,7,10,12,19],8))  #true
+print(averagePair([1,3,3,5,6,7,10,12,19],8))  #false
 # print(averagePair([-1,0,3,4,5,6], 4.1))  #false
 # print(averagePair([],4))  #false
