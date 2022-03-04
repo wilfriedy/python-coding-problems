e2dd79745ca39aee13ac06b50db5e432a3fd7b66
boydeveloper <wilfriedakpe3@gmail.com>
2022-03-04 16:26:08 +0100

    binary search algorithm implementation

--- search.py
+++ search.py
@@ -21,3 +21,30 @@
 #    5, 6, 10, 13, 14, 18, 30, 34, 35, 37,
 #    40, 44, 64, 79, 84, 86, 95, 96, 98, 99
 #  ], 100) // -1
+import math
+
+
+def binarySearch(arr, val) :
+    if(len(arr) <= 0):
+        return False
+    starts = 0
+    ends = len(arr) - 1
+    middle = math.floor((starts + ends) /2)
+    # index_count = 0
+    while arr[middle] != val and starts <= ends:
+        print(ends , 'end')
+        print(starts , 'stat')
+        print(middle , 'middle')
+        print(arr[middle] , val , 'one')
+        if arr[middle] > val:
+            ends = middle - 1
+        else:
+            starts = middle + 1
+        middle = math.floor((starts + ends) /2)
+    
+    if arr[middle] == val:
+        print('found' , arr[middle])
+        return middle
+
+
+print(binarySearch([1,2,3,5,4],5))
